0e136a6373a204291c96306f5b16cbd8b989596a
tavvfiq <taufiq.w@mail.ugm.ac.id>
2020-10-06 15:41:36 +0700

    console on user disconnect

--- src/handler/socketHandler.js
+++ src/handler/socketHandler.js
@@ -3,5 +3,8 @@ module.exports = (io) => {
 		const id = socket.handshake.query.id;
 		socket.join(id);
 		console.log("user connected with id: ", id);
+		socket.on("disconnect", () => {
+			console.log(`user with id: ${id} disconnected`);
+		});
 	});
 };
